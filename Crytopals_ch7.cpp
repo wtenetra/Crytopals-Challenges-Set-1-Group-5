983045bcb13b77a279fccf5abb786dad98e304e2
wtenetra <147451974+wtenetra@users.noreply.github.com>
2023-10-11 23:44:20 -0500

    ch7 text file

---
+++ Crytopals_ch7.cpp
@@ -0,0 +1,53 @@
+//The Cryptopals Crypto Challenges - Set 1 Challenge 7
+
+#pragma warning(disable : 4996)
+
+
+using namespace std;
+
+
+#include "Crytopals.h"
+//Follow the documentation: http://docs.huihoo.com/doxygen/openssl/1.0.1c/crypto_2aes_2aes_8h.html
+int main()
+{
+	Cryptopals crypt;
+
+	AES_KEY decryptKey;
+	int bitMode = 128; //AES-128 mode
+
+	string key = "YELLOW SUBMARINE";
+	string message;
+
+	
+	ifstream infile;
+	infile.open("ch7.txt");
+	getline(infile, message, '\0');
+	infile.close();
+	
+
+	message = crypt.b64_to_ascii(message);
+
+	//Set the decrypt key before inverting AES
+	AES_set_decrypt_key((const unsigned char*)key.c_str(), bitMode, &decryptKey);
+
+	unsigned char* decrypted = new unsigned char[message.size() + 2];
+	decrypted[message.size()] = '\0';
+
+	//Using ECB means you divide the message into blocks based on the number of bits
+	//in this case, 128 / 8 = 64 blocks;
+	int blocks = bitMode / 8;
+
+
+	for (int i = 0; i * blocks < message.size(); i++)
+	{
+		AES_ecb_encrypt((const unsigned char*)&(message.c_str()[i * blocks]), &decrypted[i * blocks], &decryptKey, AES_DECRYPT);
+	}
+
+	cout << decrypted << endl;
+	
+
+	return 0;
+
+
+	
+}
